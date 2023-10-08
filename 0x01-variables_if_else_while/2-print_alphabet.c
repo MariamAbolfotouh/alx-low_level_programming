d6f79047e89bcfad20f8e81c6567797c65442942
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2023-10-08 03:44:40 -0700

    task 02

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print the alphabet in lowercase
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	puchar('\n');
+
+	return (0);
+}
