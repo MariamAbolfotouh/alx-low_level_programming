7ac8d62d8a8319935edeed3a00df0d83119926a3
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2023-10-08 14:03:32 -0700

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * DEscription:  prints all possible different combinations of two digits
+ *
+ * REturn: Always 0 (success)
+*/
+
+int main(void)
+{
+
