29b11d81d12b2af73afe91185a9f9d9382b9e94b
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2023-10-12 15:10:51 -0700

    task 09

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,14 +10,14 @@ int main(void)
 {
 	int num;
 
-	for (num = 1; num <= 100; num++)
+	for (num = 1; num < 100; num++)
 	{
-		if (num % 3 == 0 && num % 5 == 0)
-			printf("FizzBuzz");
+		if ((num % 3) == 0 && (num % 5) == 0)
+			printf("FizzBuzz ");
 		else if (num % 5 == 0)
-			printf("Buzz");
+			printf("Buzz ");
 		else if (num % 3 == 0)
-			printf("Fizz");
+			printf("Fizz ");
 		else
 			printf("%d ", num);
 
