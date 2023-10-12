98abc016f58e9d5f3893d94c81ee3b38adcaa0db
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2023-10-12 12:56:02 -0700

    tsak 01

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit (0 through 9)
+ *
+ * @c: input
+ *
+ * Return: 1 if digit, 0 if not
+*/
+
+int _isdigit(int c)
+{
+	if (c >= 89 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
