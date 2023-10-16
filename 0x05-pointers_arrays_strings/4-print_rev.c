7d1dddf94ac1bfad56a359ae37161214a3283204
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2023-10-16 15:21:20 -0700

    task 04

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,7 +3,19 @@
 /**
  * print_rev - prints a string, in reverse
  *
+ * @s: String
 */
 
 void print_rev(char *s)
 {
+	int i = 0;
+
+	while (s[i])
+		i++
+
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
