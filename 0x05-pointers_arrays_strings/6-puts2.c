3ed49011b89df6eedb8d3e5c563213c476ba2502
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2023-10-17 11:01:16 -0700

    task 06

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,12 +11,12 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	for (i = 0; str[i] != '\0'; ++1)
 	{
 		if (i % 2 == 0)
 		{
 			_putchar(str[i]);
 		}
 	}
-	_putchar("\n");
+	_putchar('\n');
 }
