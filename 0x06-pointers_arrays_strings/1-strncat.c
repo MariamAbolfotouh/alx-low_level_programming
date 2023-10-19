c5a78df84c6a04e4ec1894658f3d1e0ab5ea7f26
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2023-10-19 10:49:10 -0700

    task 01

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,7 +2,27 @@
 
 /**
  * *_strncat - concatenates two strings
+ *
+ * @dest: input string
+ * @src: input string
+ * @n: bytes
+ *
+ * Return: to dest
 */
 
 char *_strncat(char *dest, char *src, int n)
 {
+	int i, j;
+
+	i = 0;
+
+	while (dest[i])
+		i++;
+
+	for (j = 0; j < n && src[j] != '\0'; j++)
+		dest[i + j] = src[j];
+
+	dest[i + j] = '\0';
+
+	return (dest);
+}
