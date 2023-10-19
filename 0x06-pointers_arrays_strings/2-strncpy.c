c5dfdcd2fec2acb62cbda45d0c3e90417999e579
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2023-10-19 10:56:23 -0700

    task 02

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,7 +2,26 @@
 
 /**
  * *_strncpy - copies a string
+ *
+ * @dest: input string
+ * @src: input string
+ * @n: number of bytes
+ *
+ * Return: to dest
 */
 
 char *_strncpy(char *dest, char *src, int n)
 {
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}
