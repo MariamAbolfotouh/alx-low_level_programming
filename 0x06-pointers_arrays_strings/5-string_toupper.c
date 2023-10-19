5fc8a624f6ffc8ae8f32093e5548e41694ee83fa
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2023-10-19 11:18:07 -0700

    task 05

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,7 +3,23 @@
 /**
  * *string_toupper - changes all lowercase letters of
  * a string to uppercase
+ *
+ * @str: input string
+ *
+ * Return: to string
 */
 
-char *string_toupper(char *)
+char *string_toupper(char *str)
 {
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+
+	return (str);
+}
