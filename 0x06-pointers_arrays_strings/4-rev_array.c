324e2c62fc3aaad2f8ed7db205b3248a670723bc
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2023-10-19 11:08:08 -0700

    task 04

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -2,7 +2,19 @@
 
 /**
  * reverse_array -  reverses the content of an array of integers
+ *
+ * @a: array
+ * @n: number of elements of the array
 */
 
 void reverse_array(int *a, int n)
 {
+	int i, j, temp;
+
+	for (i = 0; j = (n - 1); i < j; i++; j--)
+	{
+		temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+}
