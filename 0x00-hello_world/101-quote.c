3bf28c1482255f5cedf5917039bb67a617703939
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2023-10-20 13:06:18 -0700

    task 08

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,22 @@
-#include <unistd.h>
+#include <stdio.h>
 
-/***/
+/**
+ * main - Entry point
+ *
+ * Description: print a string using putchar function
+ *
+ * Return:  1 (Success)
+*/
 
 int main(void)
 {
+	int i;
+	char s[60] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		putchar(s[i]);
+	}
 
 	return (1);
 }
