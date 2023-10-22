d73895354784a5303efc29a6b816b5ce8a05e60a
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2023-10-22 02:59:23 -0700

    101

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints all possible combinations of two two-digit numbers
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i, j, m, n;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 48; j < 57; j++)
+		{
+			for (m = 48; m <= 57; m++)
+			{
+				for (n = 48; n <= 57; n++)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(m);
+					putchar(n);
+					if (!(i == 57 && j == 56 && m == 57 && n == 57))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
+
