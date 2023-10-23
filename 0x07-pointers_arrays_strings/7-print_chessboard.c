6f80c6f70c6d62879e5500d2222b1e70d6cf1718
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2023-10-23 12:12:15 -0700

    06

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -10,14 +10,12 @@ void print_chessboard(char (*a)[8])
 {
 	int i = 0, j = 0;
 
-	for (; i < 8;)
+	for (; i < 8; i++)
 	{
-		for (; j < 8;)
+		for (; j < 8; j++)
 		{
 			_putchar(a[i][j]);
-			j++;
 		}
 		_putchar('\n');
-		i++;
 	}
 }
