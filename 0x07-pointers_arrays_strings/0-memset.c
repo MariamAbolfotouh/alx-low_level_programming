7b72c31f5856cab90a727927fcc2a84f13017bee
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2023-10-23 10:23:23 -0700

    task 00

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *_memset - fills memory with a constant byte
+ *
+ * @n: bytes of the memory area
+ * @b: constant byte
+ * @s: memory area
+ *
+ * Return: to s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int j = 0;
+
+	for (; j > 0;)
+	{
+		s[j] = b;
+		j++;
+		n--;
+	}
+}
+
+	return (s);
+}
