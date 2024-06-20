1238ddbe564a937452315d7b88584243e919c080
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2024-06-20 15:24:28 -0700

    h

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Entry Point
+ * main - Entry point
  *
- * Describtion: a C program that prints using puts function
- *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 
-int main(void)
+int main (void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
