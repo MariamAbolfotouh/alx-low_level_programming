fb8a48cd184dfb72918c84001c484ef96a86e619
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2024-06-21 15:47:07 -0700

    dsc

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,20 +3,15 @@
 /**
  * main - Entry point
  *
- * Description: prints all single digit numbers of base 10 starting from 0
- *
- * Return: Always 0 (success)
-*/
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-	int digit = 0;
+	int i;
 
-	while (digit <= 9)
-	{
-		putchar(digit + '0');
-		digit++;
-	}
+	for (i = 48; i <= 57; i++)
+		putchar(i);
 	putchar('\n');
 	return (0);
 }
