49fe4e8069cc747fcdca89ffe3a6551398690a46
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2024-06-21 15:13:20 -0700

    df

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,27 +3,17 @@
 /**
  * main - Entry point
  *
- * Description:  prints the alphabet in lowercase, and then in uppercase
- *
- * Return: 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	char ch = 'a';
-	char CH = 'A';
+	char i;
 
-	while (ch <= 'z')
-	{
-		putchar(ch);
-		ch++;
-	}
-
-	while (CH <= 'Z')
-	{
-		putchar(CH);
-		CH++;
-	}
+	for (i = 97; i <= 122; i++)
+		putchar(i);
+	for (i = 65; i <= 90; i++)
+		putchar(i);
 	putchar('\n');
 
 	return (0);
