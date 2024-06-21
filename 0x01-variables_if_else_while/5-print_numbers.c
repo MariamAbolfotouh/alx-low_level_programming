43f8a5d269fa40e5ada2c8b33345daae20bb3808
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2024-06-21 15:35:32 -0700

    dc

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,21 +3,14 @@
 /**
  * main - Entry point
  *
- * Description: prints all single digit numbers of base 10 starting from 0
- *
- * Return: Always 0 (succes)
-*/
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-	int digit = 0;
-
-	while (digit <= 9)
-	{
-		printf("%i", digit);
-		digit++;
-	}
-	printf("\n");
-
+	int i;
+	for (i = 0; i <= 9; i++)
+		printf("%d", i);
+	putchar('\n');
 	return (0);
 }
