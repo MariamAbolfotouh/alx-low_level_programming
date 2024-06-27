b56706c06a0587969a64e862b50ccf8fc6cd504c
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2024-06-27 13:21:10 -0700

    df

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
 /**
- * print_numbers -  prints the numbers, from 0 to 9
- *
- * Return: Always 0 (success)
-*/
+ * print_numbers - prints the numbers, from 0 to 9, followed by a new line
+ */
 
 void print_numbers(void)
 {
-	int num = 0;
+	int i;
 
-	do {
-		_putchar(num + 48);
-		num++;
-	} while (num >= 0 && num <= 9);
+	for (i <= 9)
+	{
+		_putchar(i);
+	}
 	_putchar('\n');
 }
