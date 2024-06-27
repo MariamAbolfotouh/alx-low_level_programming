d39608899304d7cf866fcfce63f38fe1af69b618
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2024-06-27 13:53:32 -0700

    df

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,7 +3,7 @@
 /**
  * print_most_numbers -  prints the numbers, from 0 to 9, followed by a new line, Do not print 2 and 4
  *
- * Return: void
+ * Return: Always 0 (Success)
  */
 
 void print_most_numbers(void)
@@ -12,7 +12,7 @@ void print_most_numbers(void)
 
 	for (i = '0'; i <= '9'; i++)
 	{
-		if ((i != '2') && (i != '4'))
+		if (i != '2' && i != '4')
 		{
 			_putchar(i);
 		}
