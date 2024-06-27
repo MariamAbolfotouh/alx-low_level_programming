52c7380153bb18ad3876506045a1a5c29ca34f0f
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2024-06-27 16:19:51 -0700

    fr

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,19 +9,23 @@ void more_numbers(void)
 {
 	int i;
 	int j;
+	int n;
 
-	for ( i = '0'; i <= '9'; i++)
+	for ( n = 0; n <= 9; n++)
 	{
-		_putchar(i);
+		for ( i = '0'; i <= '9'; i++)
+		{
+			_putchar(i);
+		}
+
+		for ( j = '0'; j <= '4'; j++)
+		{
+			_putchar('1');
+			_putchar(j);
+		}
+
+		char newline = '\n';
+
+		write(1, &newline, 1);
 	}
-
-	for ( j = '0'; j <= '4'; j++)
-	{
-		_putchar('1');
-		_putchar(j);
-	}
-
-	char newline = '\n';
-
-	write(1, &newline, 1);
 }
