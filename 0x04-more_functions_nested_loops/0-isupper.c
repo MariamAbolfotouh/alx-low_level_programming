a3b1444e219d176613542efc2c5bd0ce9deac360
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2024-06-27 12:33:18 -0700

    df

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,11 @@
 #include <main.h>
 
 /**
- * _isupper :  a function that checks for uppercase character
+ * _isupper - a function that checks for uppercase character
  *
  * @c : is the uppercase letter
  *
- * return: (1) if c is uppercase, (0) otherwise
+ * Return: 1 if c is uppercase, 0 otherwise
  */
 
 int _isupper(int c)
