bd55b4af9ff51529e295fb2f7a8226d001fae948
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2024-06-27 13:56:36 -0700

    f

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,20 +2,17 @@
 
 /**
  * print_most_numbers -  prints the numbers, from 0 to 9, followed by a new line, Do not print 2 and 4
- *
- * Return: Always 0 (Success)
  */
 
 void print_most_numbers(void)
 {
-	int i;
+	int num;
 
-	for (i = '0'; i <= '9'; i++)
+	for (num = 0; num <= 9; num++)
 	{
-		if (i != '2' && i != '4')
-		{
-			_putchar(i);
-		}
+		if (num == 2 || num == 4)
+			continue;
+		_putchar(num + 48);
 	}
 	_putchar('\n');
 }
