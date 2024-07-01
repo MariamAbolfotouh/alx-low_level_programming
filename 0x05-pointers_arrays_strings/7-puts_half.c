05edff29af9bd552c529126cd7230329986546ac
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2024-07-01 16:27:51 -0700

    fd

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -3,15 +3,15 @@
 /**
  * puts_half - prints half of a string
  *
- * @n: characters of the string
+ * @str: input string
  */
 
 void puts_half(char *str)
 {
 	int i;
 	int n;
-
 	int len = 0;
+
 	while (str[len] != '\0')
 	{
 		len++;
@@ -29,7 +29,7 @@ void puts_half(char *str)
 	{
 		n = (len - 1) / 2;
 		for (i = n; str[i] != '\0'; i++)
-		{	
+		{
 			_putchar(str[i]);
 		}
 	}
