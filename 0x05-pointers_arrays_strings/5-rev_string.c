54e7de5b5579dd613d0ecc2961677130bda26025
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2024-07-01 17:19:12 -0700

    df

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,4 +8,16 @@
 
 void rev_string(char *s)
 {
+	int i;
+	char temp;
 
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (int j = 0; i > j; i--, j++)
+		{
+			temp = s[i];
+			s[i] = s[j];
+			s[j] = temp;
+		}
+	}
+}
