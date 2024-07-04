7502fb6f4e26e943d90fa02bf182f8aa1a75d188
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2024-07-04 14:08:32 -0700

    df

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,7 +2,30 @@
 
 /**
  * *cap_string - capitalizes all words of a string
+ *
+ * @str: input
+ *
+ * Return: str
 */
 
-char *cap_string(char *)
+char *cap_string(char *str)
 {
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ' || str[i] == '\n' || str[i] == '\t' || str[i] == '.')
+		{
+			i++;
+			if (str[i] >= 65 && str[i] <= 90)
+			{
+				continue;
+			}
+			else if (str[i] >= 97 && str[i] <= 122)
+			{
+				str[i] = str[i] - 32;
+			}
+		}
+	}
+	return (str);
+}
