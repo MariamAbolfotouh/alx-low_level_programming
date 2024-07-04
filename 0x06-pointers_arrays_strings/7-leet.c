ff8c9429e0631c9ee6f7752c6c9e9c0af748b70d
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2024-07-04 16:40:12 -0700

    df

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,12 +3,26 @@
 /**
  * leet - encodes a string into 1337
  *
- * @c: string
+ * @n: input value
  *
  * Return: string
 */
 
-char *leet(char *)
+char *leet(char *n)
 {
-	char *cp = c;
+	int i, j;
+	char s1[] = "AaEeOoTtLl"
+	char s2[] = "4433007711"
 
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (n[i] == s1[j])
+			{
+				n[i] = s2[j];
+			}
+		}
+	}
+	return (n);
+}
