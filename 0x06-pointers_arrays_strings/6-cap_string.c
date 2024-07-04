1fd23553cbc62df943036503f88255737f8edc70
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2024-07-04 16:51:41 -0700

    df

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,28 +14,18 @@ char *cap_string(char *str)
 
 	while (str[i] != '\0')
 	{
-		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
-		str[i] == ',' || str[i] == ';' || str[i] == '.' ||
-		str[i] == '!' || str[i] == '?' || str[i] == '"' ||
-		str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
-		{
-			i++;
-			while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
-			str[i] == ',' || str[i] == ';' || str[i] == '.' ||
-			str[i] == '!' || str[i] == '?' || str[i] == '"' ||
-			str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
-			{
-				i++;
-			}
-			if (str[i] >= 'a' && str[i] <= 'z')
-			{
-				str[i] = str[i] - ('a' - 'A');
-			}
-		}
-		else
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
 		{
 			i++;
 		}
+		if (str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
+		str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
+		str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' ||
+		str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}' || i == 0)
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
 	}
 	return (str);
 }
