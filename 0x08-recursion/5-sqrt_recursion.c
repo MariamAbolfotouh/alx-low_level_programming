9706dd62dfb94f3fa894a125a7184b0624efe4bf
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2024-07-11 15:12:20 -0700

    fg

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ *
+ * @n: number
+ *
+ * Return: -1 If n does not have a natural square root
+ */
+
+int guess(int n, int sqr)
+{
+	if (n = sqr * sqr)
+	{
+		return (sqr);
+	}
+	else if (sq < n)
+	{
+		return (guess(n, ++sqr);
+	}
+	else
+	{
+	return (-1)
+	}
+}
+
+int _sqrt_recursion(int n)
+{
+	int guess(int n, int sqr);
+
+	int sqr = 1;
+	return (_sqrt_recursion(n, ++srq));
+}
