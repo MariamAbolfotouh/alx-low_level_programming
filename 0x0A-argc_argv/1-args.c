039377c13bdda1ae01be1ecdbfa44cbc3e4b5525
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2024-07-14 14:38:31 -0700

    f

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main _ Entry point
+ *
+ * @argc: count
+ * @argv: vector
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
