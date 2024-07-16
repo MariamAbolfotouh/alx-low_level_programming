8975cbfc6914eea045babdb902fe3b50f2bf28b5
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2024-07-16 16:39:17 -0700

    fg

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -22,7 +22,7 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		p[i] = calloc(width, sizeof(int));
+		p[i] = malloc(width * sizeof(int));
 		if (p[i] == NULL)
 		{
 			for (j = 0; j < i; j++)
