ff7354eed300e09784ca231db5af2a47e00bdda5
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2024-07-16 13:47:55 -0700

    fg

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
 	char temp = 'c';
-	char *p =(malloc(size * sizeof(char));
+	char *p = malloc(size * sizeof(char));
 
 	for (i = 0; i < size; i++)
 	{
