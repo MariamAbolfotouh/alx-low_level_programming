dcab765b8c660d47d97c0942aa6b6f725a4ed385
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2024-07-17 15:31:32 -0700

    fg

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,25 +12,26 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
+	int i, j;
 	int sum = 0;
 
-	if (atoi(argv[i]) > 0)
-	{
-		for (i = 1; i < argc; i++)
-		{
-			sum = sum + atoi(argv[i]);
-		}
-		printf("%d\n", sum);
-	}
-	else if (argc = 1)
+	if (argc == 1)
 	{
 		printf("0\n");
 	}
-	else if (atoi(argv[i]) < 0 || atoi(argv[i]) > 9)
+
+	for (i = 1; i < argc; i++)
 	{
-		printf("Error\n");
-		return (1);
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
 	}
+	sum = sum + atoi(argv[i]);
+	printf("%d\n", sum);
 	return (0);
 }	
