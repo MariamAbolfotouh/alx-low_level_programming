eaccf06f5d01bfca9c47ade45119562bfd74f930
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2024-07-18 14:55:35 -0700

    g

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -37,6 +37,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	ptr = malloc((strlen_1 + n + 1) * sizeof(char));
 
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 		ptr[i] = s1[i];
