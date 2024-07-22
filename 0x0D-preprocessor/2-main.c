93bc0451ad2974c3036f8c1b0dbe3aff199c9b12
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2024-07-22 15:06:31 -0700

    jg

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#ifndef _main_c
+#define _main_c
+
+#define __file__
+
+#endif
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
