657458e84aa6b9c0bfa6d8da390c2ef9e76ae1a1
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2024-07-23 14:37:52 -0700

    fg

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#ifndef dog_h
+#define dog_h
+
+/**
+ * init_dog - initialize a variable of type struct dog
+ *
+ * @d: name of struct
+ * @name: member 1
+ * @age: member 2
+ * @owner: member 3
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
