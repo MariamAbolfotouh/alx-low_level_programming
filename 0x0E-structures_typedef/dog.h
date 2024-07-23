12a8c8257eb191bf5cfce87bb0d749f098d7bb21
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2024-07-23 14:20:34 -0700

    df

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * struct dog - a dog struct
+ *
+ * @name: member 1
+ * @age: member 2
+ * @owner: member 3
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
