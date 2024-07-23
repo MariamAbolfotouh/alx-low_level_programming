51ab8d7f77523ba250857e9cfdcc738c8df01ac2
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2024-07-23 16:51:57 -0700

    fd

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -68,7 +68,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	dog->name = malloc(sizeof(char) * (strlen(name) + 1));
+	dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
 
 	if ((*dog).name == NULL)
 	{
@@ -76,7 +76,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
+	dog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
 
 	if ((*dog).owner == NULL)
 	{
