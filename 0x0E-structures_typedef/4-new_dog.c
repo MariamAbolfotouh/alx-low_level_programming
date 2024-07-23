1bfe2e83bd2d9bcb03191d0c2dfff909b9498636
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2024-07-23 16:15:49 -0700

    fg

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -53,5 +53,36 @@ char *_strcopy(char *dest, char *src)
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	dog_t *dog;
+
+	if (name == NULL || age < 0 || owner == NULL)
+	{
+		return (NULL);
+	}
+
+	dog = (dog_t *) malloc(sizeof(dog_t));
+
+	if (dog == NULL)
+	{
+		return (NULL);
+	}
+
+	dog->name = malloc(sizeof(char) * (strlen(name) + 1));
+
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+
+	dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
+
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(fog);
+		return (NULL);
+	}
+}
 
 
