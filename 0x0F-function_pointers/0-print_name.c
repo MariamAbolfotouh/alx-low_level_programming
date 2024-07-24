895f12903cfc868acf788075bd14c0667defb0be
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2024-07-24 14:21:54 -0700

    df

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_name - prints a name
+ *
+ * @name: pointer
+ * @f: pointer to function
+ *
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+	{
+		f(name);
+	}
+}
