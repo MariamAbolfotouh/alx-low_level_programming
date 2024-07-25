eaea7e4ad2d88b36587ca4effa890f2181217d21
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2024-07-24 17:19:56 -0700

    fg

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,36 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func - selects the correct function
+ * to perform the operation asked by the user
+ *
+ * @s: the operator passed as argument to the program
+ * @a: 1st arg
+ * @b: 2nd arg
+ *
+ * Return: a pointer to the function that corresponds
+ * to the operator given as a parameter
+ */
+
+int (*get_op_func(char *s))(int a, int b)
+{
+	op_t ops[] = {
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+	};
+	int i = 0;
+
+	while (i < 5)
+	{
+		if (s && s[0] == ops[i].op[0] && !s[1])
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,71 @@
+#include "3-calc.h"
+
+/**
+ * op_add - the sum of a and b
+ *
+ * @a: 1st number
+ * @b: 2nd number
+ *
+ * Return: the sum of a and b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - difference of a and b
+ *
+ * @a: 1st number
+ * @b: 2nd number
+ *
+ * Return: difference of a and b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - product of a and b
+ *
+ * @a:1st num
+ * @b: 2nd num
+ *
+ * Return: product of a and b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division of a by b
+ *
+ * @a: 1st num
+ * @b: 2nd num
+ *
+ * Return: result of the division of a by b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - the remainder of the division of a by b
+ *
+ * @a: 1st num
+ * @b: 2nd num
+ *
+ * Return: the remainder of the division of a by b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
