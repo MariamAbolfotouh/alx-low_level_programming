04b96bd544033b0ca7a1edf603b97928931844e2
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2024-07-25 15:10:02 -0700

    fg

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,10 +17,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_start(ptr, n);
 
-	str = va_arg(ptr, *char);
-
 	for (i = 0; i < n; i++)
 	{
+		str = va_arg(ptr, *char);
+
 		if (str != NULL)
 		{
 			printf("%s", str);
