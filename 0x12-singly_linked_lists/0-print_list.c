9c18700c77fb9baa56297d98604ec01217ee257e
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2024-08-01 14:32:19 -0700

    fg

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,27 +1,6 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a list_t list
- *
- * @h: pointer to 1st node
- *
- * Return: list size
- */
-
-size_t print_list(const list_t *h)
-{
-	size_t i = 0;
-
-	while (h)
-	{
-		printf("[%d] %s\n", _strlen(h->str), h->str : "(nil)");
-		h = h->next;
-		i++;
-	}
-	return (i);
-}
-
-/**
  * _strlen - get the string length
  *
  * @s: string
@@ -38,6 +17,28 @@ int _strlen(char *s)
 	}
 	while (s[i] != '\0')
 	{
+		i++;
+	}
+	return (i);
+}
+
+
+/**
+ * print_list - prints all the elements of a list_t list
+ *
+ * @h: pointer to 1st node
+ *
+ * Return: list size
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", _strlen(h->str), h->str : "(nil)");
+		h = h->next;
 		i++;
 	}
 	return (i);
