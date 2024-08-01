bb14d9d040e1df76e716704cf1a30e6085972837
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2024-08-01 15:15:23 -0700

    df

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,28 +11,36 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node = malloc(sizeof(list_t));
-	list_t *node = *head;
+	list_t *new_node;
+	list_t *last_node;
 
-	if (!head || !new_node)
+	if (head == NULL || str == NULL)
 		return (NULL);
-	if (str)
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
 	{
-		new_node->str = strdup(str);
-		if (!new_node->str)
-		{
-			free(new_node);
-			return (NULL);
-		}
-		new_node->len = _strlen(new_node->str);
+		free(new_node);
+		return (NULL);
 	}
-	if (node)
+
+	new_node->len = _strlen(new_node->str);
+	new_node->next = NULL;
+
+	if (*head == NULL)
 	{
-		while (node->next)
-			node = node->next;
-		node->next = new_node;
-	}
-	else
 		*head = new_node;
+		return (new_node);
+	}
+
+	last_node = *head;
+	while (last_node->next != NULL)
+	last_node = last_node->next;
+
+	last_node->next = new_node;
 	return (new_node);
 }
