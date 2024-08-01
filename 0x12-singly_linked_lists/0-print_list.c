6f8b01bd4b0991f8a0e3b294ad848414d0e7a2d4
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2024-08-01 14:28:20 -0700

    dg

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ *
+ * @h: pointer to 1st node
+ *
+ * Return: list size
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", _strlen(h->str), h->str : "(nil)");
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * _strlen - get the string length
+ *
+ * @s: string
+ *
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (s == NULL)
+	{
+		return (0);
+	}
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
