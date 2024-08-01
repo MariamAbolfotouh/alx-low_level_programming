2a9e5d9084101fffe69d9f4cea1555be6b767190
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2024-08-01 14:59:33 -0700

    fd

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,23 +11,25 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new = malloc(sizeof(list_t));
+	list_t *new_node;
 
-	if (!head || !new)
+	if (head == NULL || str == NULL)
+	return (NULL);
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	return (NULL);
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
 	{
+		free(new_node);
 		return (NULL);
 	}
-	if (str)
-	{
-		new->str = strdup(str);
-		if (!new->str)
-		{
-			free(new);
-			return (NULL);
-		}
-		new->len = _strlen(new->str);
-	}
-	new->next = *head;
-	*head = new;
-	return (new);
+
+	new_node->len = _strlen(new_node->str);
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
 }
