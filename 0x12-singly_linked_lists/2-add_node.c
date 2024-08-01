ea74ea5d6a2d4afa59ac303cdc50d817ad881181
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2024-08-01 14:50:21 -0700

    df

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning of a list_t list.
+ *
+ * @head: beginning node of a list_t list.
+ * @str: needs to be duplicated
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new = malloc(sizeof(list_t));
+
+	if (!head || !new)
+	{
+		return (NULL);
+	}
+	if (str)
+	{
+		new->str = strup(str);
+		if (!new->str)
+		{
+			free(new);
+			return (NULL);
+		}
+		new->l = _strlen(new->str);
+	}
+	new->next = *head;
+	*head = new;
+	return (new);
+}
