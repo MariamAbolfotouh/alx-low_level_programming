6af400941c6b270f3013cd6efd0aead642f3da3d
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2024-08-12 14:36:21 -0700

    df

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,6 @@
+#include "main.h"
+
+#define Usage "Usage: cp file_from file_to\n"
+#define Error_R "Error: Can't read from file %s\n"
+#define Error_W "Error: Can't write to %s\n"
+#define Error_C "Error: Can't close fd %d\n"
