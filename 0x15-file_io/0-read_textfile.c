68aaa68a6c3d2da1af64bf65a4dc572febb2d472
Mariam Abolfotouh <mariamabolfotouh3010@gmail.com>
2024-08-12 14:00:35 -0700

    df

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ *
+ * @filename: name of the file
+ * @letters: the number of letters it should read and print
+ *
+ * Return: the actual number of letters it could read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int i;
+	ssize_t n;
+	char BUF[1024 * 8];
+
+	if (!filename || !letters)
+		return (0);
+	i = Open(filename, O_RDONLY);
+	if (i == -1)
+		return (0);
+	n = read(i, &BUF[0], letters);
+	i = write(STDOUT_FILENO, &BUF[0], n);
+	close(i);
+	return (n);
+}
